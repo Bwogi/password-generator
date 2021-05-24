0f9d5d45202605d5b2013ebd6facdf4ebf16c6db
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-05-23 20:39:57 -0700

    final js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,70 @@ var passwdTxt = document.getElementById("result");
 var alphanumerals = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*_-+=";
+var characters = "";
 
-
+var charCheck = "";
+var numberCheck = "";
+var symbolCheck = "";
+var passwordLength = 0;
 
 document.getElementById('genButton').addEventListener("click", function() {
-    var passwordLength = prompt("What is the length of your password?");
+    passwordLength = prompt("What is the length of your password?");
 
     if (passwordLength > 8 && passwordLength < 128) {
-        var charCheck = confirm("Should it include lower and upper case characters?!");
-        var numberCheck = confirm("Should it include numbers?!");
-        var symbolCheck = confirm("Should it include symbols?!");
+        // should it include chars ?
+        charCheck = confirm("Should it include lower and upper case characters?!");
+        // yes - > add alphanumerals to characters -
         if (charCheck) {
-            var characters = alphanumerals;
+            characters += alphanumerals;
+            // > should it include numbers ?
+            numberCheck = confirm("Should it include numbers?!");
+            if (numbers) {
+                // yes - > add numbers to characters
+                characters += numbers;
+                symbolCheck = confirm("Should it include symbols?!");
+                if (symbolCheck) {
+                    characters += symbols;
+                    passwdTxt.value = genPasswd(passwordLength, characters);
+                }
+
+            } else {
+                symbolCheck = confirm("Should it include symbols?!");
+                if (symbolCheck) {
+                    characters += symbols;
+                    passwdTxt.value = genPasswd(passwordLength, characters);
+                }
+
+            }
+
+
+        } else {
+            numberCheck = confirm("Should it include numbers?!");
             if (numberCheck) {
                 characters += numbers;
+                symbolCheck = confirm("Should it include symbols?!");
+                if (symbolCheck) {
+                    characters += symbols;
+                    passwdTxt.value = genPasswd(passwordLength, characters);
+                }
+
+
+            } else {
+                if (symbolCheck) {
+                    characters += symbols;
+                    passwdTxt.value = genPasswd(passwordLength, characters);
+                }
+
             }
-            if (symbolCheck) {
-                characters += symbols;
-            }
-            passwdTxt.value = genPasswd(passwordLength, characters);
+
         }
+
     } else {
         alert("Length of password must be between 8 and 128 characters")
     }
 
+
+    // generate password 
     function genPasswd(length, xters) {
         let password = "";
         for (let i = 0; i < length; i++) {
